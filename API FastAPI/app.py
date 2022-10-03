445a7e66709ee7c80e941fad3072660ae8c189c0
leandroirigoyen <leandro.irigoyen97@gmail.com>
2022-10-03 19:38:25 -0300

    hola

--- API FastAPI/app.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes.user import user
-from routes.event import event
-
-app = FastAPI()
-
-app.include_router(user)
-app.include_router(event)
--- API FastAPI/config/db.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, MetaData
-
-engine = create_engine("mysql+pymysql://root:@localhost:3306/storedb")
-
-meta = MetaData()
-
-conn = engine.connect()

3ed808bf237e18b0e01b20e079c65edd3be93486
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-11 11:05:04 -0300

    timedate

--- API FastAPI/schemas/event.py
+++ API FastAPI/schemas/event.py
@@ -3,15 +3,14 @@ from typing import Optional
 from pydantic import BaseModel
 
 class EventSchema(BaseModel):
-    name: str
+    name: str = ""
 
     event_host: int
     event_datetime: datetime = (datetime.now() + timedelta(hours=1))
-    location: str
-    description: str
+    location: str = "No location"
+    description: str = "Description"
     icon: str = ""
     max_people: int = 1
     participants: str
     
     config: dict = None
- 
--- API FastAPI/schemas/user.py
+++ API FastAPI/schemas/user.py
@@ -2,9 +2,9 @@ from typing import Optional
 from pydantic import BaseModel
 
 class UserSchema(BaseModel):
-    name: str
-    email: str
-    password: str
-    phone: str
+    name: str = ""
+    email: str = ""
+    password: str = ""
+    phone: str = ""
 
     

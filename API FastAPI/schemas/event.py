27e987df1720b6dcb2d90dffdf720cc7e397556f
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-14 17:10:43 -0300

    tiyding things up

--- API FastAPI/schemas/event.py
+++ API FastAPI/schemas/event.py
@@ -4,15 +4,15 @@ from typing import Optional, Dict, List, Union
 from pydantic import BaseModel
 
 class EventSchema(BaseModel):
-    name: str = ""
+    name: str = "Event name"
 
     event_host_id: int
     event_datetime: datetime = (datetime.now() + timedelta(hours=1))
-    location: str = "No location"
+    location: str = "Location"
     description: str = "Description"
     icon: str = ""
     max_people: int = 1
-    participants: List[int]
+    participants: List[int] = []
     
     config: Dict[str, Union[None, int, bool]] = {
         "online": False, 

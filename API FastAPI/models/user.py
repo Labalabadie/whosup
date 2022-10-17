9ed93eae89b57622657dfbf08865c4597cbde1a5
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-17 15:35:45 -0300

    f!

--- API FastAPI/models/user.py
+++ API FastAPI/models/user.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.sqltypes import Integer, String, DateTime, Boolean
 from datetime import datetime
 from models.base_model import BaseModel
-from models.event import attending_event_rel
+from models.event import AttendingEventRel
 
 class User(BaseModel):
     __tablename__ = "user_data"
@@ -19,7 +19,7 @@ class User(BaseModel):
 
     # Relationships --
     hosted_events = relationship('Event', back_populates='event_host')
-    attending_events = relationship("Event", secondary=attending_event_rel, back_populates='participants')
+    attending_events = relationship("Event", secondary=AttendingEventRel, back_populates='participants')
     admin_groups = relationship('Group', back_populates='group_admin')
     admin_channels = relationship('Channel', back_populates='channel_admin')
 

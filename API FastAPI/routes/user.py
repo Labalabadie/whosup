5fc4c099c9e747915e374841d9317321aff5ff04
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-17 22:13:56 -0300

    ?

--- API FastAPI/routes/user.py
+++ API FastAPI/routes/user.py
@@ -53,7 +53,7 @@ def get_user_info(id: int):
     admin_channels_list = conn.execute(select(User.admin_channels, Channel).join(Channel).where(User.id == id)).all()
     admin_groups_list = conn.execute(select(User.admin_groups, Group).join(Group).where(User.id == id)).all()
 
-    print(User.to_dict)
+    print(User.to_dict())
     return json.dumps(User._get())
 
 

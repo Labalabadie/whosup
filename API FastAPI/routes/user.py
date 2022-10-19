d8b382e05c4dfed546c1affb1cc47b6b2fd1b330
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-19 19:26:32 -0300

    asdf

--- API FastAPI/routes/user.py
+++ API FastAPI/routes/user.py
@@ -40,8 +40,7 @@ def get_feed(id: int):
     events_feed = conn.execute(select(Event)
                         .select_from(User)
                         .join(User.attending_events)
-                        .filter(Event.event_host_id != id)
-                        .filter(User.name == 'Laba')
+                        .filter(Event.event_host_id != id, User.name == 'Laba')
                         .where(Event.status == True)).all()
 
     hosted_events_list = conn.execute( # One to many relationship join query

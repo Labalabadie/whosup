58a28b9c16f4244f7f79bd5b0ee24f59d079a8aa
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-22 12:52:23 -0300

    tes

--- API FastAPI/routes/user.py
+++ API FastAPI/routes/user.py
@@ -38,13 +38,6 @@ def get_feed(id: int):
     events_query = sess.query(Event).join(User, Event.participants)
     events_feed = events_query.filter(not_(or_(Event.event_host_id == id, User.id == id)))
 
-    events_feed_list = conn.execute((Event, attending_event_rel)
-                        .select_from(User)
-                        .join(User.attending_events)                    # Exclude from feed all events...
-                        .filter(not_(or_(Event.event_host_id == id,     # hosted by cur.user,
-                                         User.id == id                  # attended by cur.user
-                                         )))
-                        .where(Event.status == True)).all()
     print(events_feed)
     hosted_events_list = conn.execute( # One to many relationship join query
                         select(User.hosted_events, Event) 

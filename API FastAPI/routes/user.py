079f6b167a13efc3983e4628e685f15b9b853ce9
Mauricio Heller <hellerdejanuar@gmail.com>
2022-10-22 21:28:36 -0300

    test

--- API FastAPI/routes/user.py
+++ API FastAPI/routes/user.py
@@ -35,10 +35,11 @@ attending_events_qry = (select(attending_event_rel, Event) # Many to many relati
 @userAPI.get('/user/{id}/feed', response_model=List[EventSchema], tags=["Users"])
 def get_feed(id: int):
     """ get feed of specified user """
-    query = sess.query(Event).join(User, Event.participants).filter(not_(or_(Event.event_host_id == id, User.id == id)))
+    query = sess.query(Event).join(User, Event.participants, isouter=True).filter(not_(or_(Event.event_host_id == id, User.id == id)))
     events_feed = query.all()
 
-    print(events_feed..all().items())
+    print(len(events_feed))
+    
     hosted_events_list = conn.execute( # One to many relationship join query
                         select(User.hosted_events, Event) 
                         .join(Event)
